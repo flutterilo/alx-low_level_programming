4afd80289ab1f226a64d38f99f851797b3a9ef18
ilyass <ilias.kassimi@gmail.com>
2023-03-17 13:40:02 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - this function will get last digit
+ * Description: this is description of function
+ * Return: return 1
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 97; i < 122; ++i)
+	{
+		putchar(i);
+	}
+	return (0);
+}
