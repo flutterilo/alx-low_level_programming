a1abf959163df29536053a7b61fa0a7d9e213016
ilyass <ilias.kassimi@gmail.com>
2023-03-17 15:12:39 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - this function will get last digit
+ * Description: this is description of function
+ * Return: return 1
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		if (i == 'e' || i == 'q')
+		{
+
+		} else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
