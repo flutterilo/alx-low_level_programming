bbe183cb790ac12a3614b62e19bd77292c1136d9
ilyass <ilias.kassimi@gmail.com>
2023-03-17 15:34:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - this function will get last digit
+ * Description: this is description of function
+ * Return: return 1
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
