01653da445c32a0c3bf3c1620c0079fd753f64a2
ilyass <ilias.kassimi@gmail.com>
2023-03-17 15:17:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - this function will get last digit
+ * Description: this is description of function
+ * Return: return 1
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
