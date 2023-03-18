4f484cb2d53b03cbda5c284618242bbdd2f3e8ea
ilyass <ilias.kassimi@gmail.com>
2023-03-17 18:05:15 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - this function will get last digit
+ * Description: this is description of function
+ * Return: return 1
+ */
+
+int main(void)
+{
+	int i;
+	int digit_two = 1;
+
+	for (i = 0; i <= 8; ++i)
+	{
+		for (int j = digit_two; j <= 9; ++j)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+			{
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		digit_two++;
+	}
+
+	putchar('\n');
+	return (0);
+}
