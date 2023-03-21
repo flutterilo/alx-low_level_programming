647afb1c0ee9f79520b84f5351f6d64acfa6c96d
Ilyass <ilias.kassimi@gmail.com>
2023-03-21 23:44:41 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _abs - make number absolute
+ * @n: parameter of function _abs
+ * Return: return depend on case
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
