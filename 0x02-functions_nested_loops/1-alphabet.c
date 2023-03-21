f396f1cdefc9ecaf233c1201222ddeb4c097cb60
Ilyass <ilias.kassimi@gmail.com>
2023-03-21 19:28:03 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function print alphabet
+ * Return: return always 0
+ */
+
+int main(void)
+{
+	char i = 'a';
+
+	while (i < 'z')
+	{
+	_putchar(i);
+	i++;
+	}
+	_putchar('\n');
+
+	return (0);
+
+}
