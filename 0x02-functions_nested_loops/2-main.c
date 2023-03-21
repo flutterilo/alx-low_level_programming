25c43be7b1a27a975ad665d69a1f375761219cb3
Ilyass <ilias.kassimi@gmail.com>
2023-03-21 20:22:25 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - print 10 times alphabet
+ * Return: return 0
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times alphabet
+ * Return: return nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int loop = 0;
+	char alpha = 'a';
+
+	while (loop <= 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		alpha = 'a';
+
+	loop++;
+	}
+
+}
