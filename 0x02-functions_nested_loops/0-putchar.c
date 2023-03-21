719dd8fcf5a642e36f8484d3b25df23f97b30a12
Ilyass <ilias.kassimi@gmail.com>
2023-03-21 15:45:49 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - main function will print a text
+ * Description: description of this function
+ * Return: it's return success with number 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+}
