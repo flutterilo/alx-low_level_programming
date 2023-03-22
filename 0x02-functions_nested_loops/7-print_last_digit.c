a5d04ef66ca0b49ac98ceacd6cf5f5decfc2911d
Ilyass <ilias.kassimi@gmail.com>
2023-03-22 00:57:01 +0000

    7-print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,8 @@ int print_last_digit(int n)
 {
 	int abs_value, last_digit;
 
-	abs_value = abs(n);
-	last_digit = abs_value % 10;
-	_putchar(last_digit + '0');
-	return (last_digit);
+	last_digit = n % 10;
+	abs_value = abs(last_digit);
+	_putchar(abs_value + '0');
+	return (abs_value);
 }
