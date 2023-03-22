4f3e87b0916e446d70055e7da88f7e635c53b82d
Ilyass <ilias.kassimi@gmail.com>
2023-03-22 22:37:28 +0000

    103-fibanocci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,14 +7,18 @@
 
 int main(void)
 {
-	long a = 0, b = 1, sum;
+	long a = 0, b = 1, sum, even_value = 0;
 
 	while ((a + b) < 4000000)
 	{
 		sum = a + b;
 		a = b;
 		b = sum;
+		if ((sum % 2) == 0)
+		{
+			even_value += sum;
+		}
 	}
-	printf("%lu\n", sum);
+	printf("%lu\n", even_value);
 	return (0);
 }
