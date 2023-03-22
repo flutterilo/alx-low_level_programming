6a27f4ff815b2bc96d6204d83f66fcfc22a62d47
Ilyass <ilias.kassimi@gmail.com>
2023-03-22 18:29:55 +0000

    102-fabonici.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - print 50 fabonici numbers
  * Return: return always 0
  */
@@ -9,12 +9,17 @@ int main(void)
 {
 	long i, a = 0, b = 1, sum;
 
-	for (i = 1; i <= 50; i++)
+	for (i = 1; i < 50; i++)
 	{
+		sum = a + b;
+		printf("%lu, ", sum);
+		a = b;
+		b = sum;
+	}
 		sum = a + b;
 		printf("%lu\n", sum);
 		a = b;
 		b = sum;
-	}
+	printf("\n");
 	return (0);
 }
