d3b9a82662654f11ae7e9d15140c396762e32f51
Ilyass <ilias.kassimi@gmail.com>
2023-03-22 04:19:36 +0000

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - times table
+ * Return: retun always 0
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+
+/**
+ * print_number - print number
+ * @num: parameter num.
+ * Return: return nothing
+ */
+
+void print_number(int num)
+{
+	if (num > 9)
+	{
+		_putchar(' ');
+		_putchar((num / 10) + '0');
+		_putchar((num % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+	} else
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((num % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
+
+
+/**
+ * times_table - print time table
+ * Return: return nothing
+ */
+
+void times_table(void)
+{
+	int first, tmp;
+
+	tmp = 0;
+	for (first = 0; first <= 9; first++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar((tmp += first) + '0');
+		_putchar(',');
+		print_number(tmp += first);
+		print_number(tmp += first);
+		print_number(tmp += first);
+		print_number(tmp += first);
+		print_number(tmp += first);
+		print_number(tmp += first);
+		print_number(tmp += first);
+		tmp += first;
+		if (tmp > 9)
+		{
+			_putchar(' ');
+			_putchar((tmp / 10) + '0');
+			_putchar((tmp % 10) + '0');
+		} else
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(tmp + '0');
+		}
+		tmp = 0;
+		_putchar('\n');
+	}
+
+
+}
