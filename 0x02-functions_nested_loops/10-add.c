fe06205f17cfed03542b0396de1ed621253e48bf
Ilyass <ilias.kassimi@gmail.com>
2023-03-22 13:09:12 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two number and get result
+ * @a: parameter a
+ * @b: parameter b
+ * Return: return result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print result of adds
+ * Return: return always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+
+}
