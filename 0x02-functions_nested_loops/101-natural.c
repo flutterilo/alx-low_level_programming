8accc31a961123e78256d8aa1e4342ba05cb43c3
Ilyass <ilias.kassimi@gmail.com>
2023-03-22 17:02:16 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 
 /**
- * sum_multiples - printing sum of multiples 3 and 5
- * Return: return nothing 
+ * main - printing sum of multiples 3 and 5
+ * Return: always return 0.
  */
 
-void sum_multiples(void)
+int main(void)
 {
 	int a, b, result1, result2, total;
-	for(a = 0; a < 1024; a += 3)
+
+	for (a = 0; a < 1024; a += 3)
 	{
 		result1 += a;
 	}
-	for(b = 0; b < 1024; b += 5)
+	for (b = 0; b < 1024; b += 5)
 	{
 		result2 += b;
 	}
 	total = result1 + result2;
 	printf("%d", total);
-
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -21,5 +21,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void sum_multiples(void);
 #endif
