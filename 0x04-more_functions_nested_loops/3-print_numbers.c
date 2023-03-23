633cd5269ef16441b85dd9d2a07d544c4e8b90f5
Ilyass <ilias.kassimi@gmail.com>
2023-03-23 23:30:21 +0000

    3-print_number.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - check if character is uppercase
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+
+}
