51e0d3a46a51807c322ddafc103f1c35101184d9
Ilyass <ilias.kassimi@gmail.com>
2023-03-23 23:22:33 +0000

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check if character is uppercase
+ * @a: a is paramemter
+ * @b: b is paramemter
+ * Return: return 1 if uppcase and 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
