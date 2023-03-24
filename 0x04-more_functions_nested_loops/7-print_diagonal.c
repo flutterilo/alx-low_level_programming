2a94b10b926f37218126b31a6607d2ad6d8e6dfa
Ilyass <ilias.kassimi@gmail.com>
2023-03-24 00:50:49 +0000

    7-prnt_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,31 @@
 #include "main.h"
 
 /**
- * print_diagonal - check if character is uppercase
- * @n: parameter n
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
-
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
+		int i, j;
+
 		for (i = 0; i < n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 0; j < n; j++)
 			{
-			_putchar(' ');
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
+				}
 			}
-			_putchar('\\');
 			_putchar('\n');
 		}
 	}
