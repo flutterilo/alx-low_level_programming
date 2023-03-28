6a370009f8de5eb8de850c72b58b206c7be28fc0
Ilyass <ilias.kassimi@gmail.com>
2023-03-28 02:45:05 +0000

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strcpy - copy a string
+  * @dest: dest parameter
+  * @src: src parameter
+  *
+  * Return: return 0 always
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
