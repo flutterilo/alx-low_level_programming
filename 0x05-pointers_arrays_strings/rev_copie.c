a9ceae4825a2b35cead55c42866efed7cd890abc
Ilyass <ilias.kassimi@gmail.com>
2023-03-28 03:47:05 +0000

    correct

--- 0x05-pointers_arrays_strings/rev_copie.c
+++ 0x05-pointers_arrays_strings/rev_copie.c
@@ -10,17 +10,21 @@ void rev_string(char *s)
 {
 	int length;
 	char *ptr;
-	char tems[1000000];
+	char *tems;
 
 	for (length = 0; s[length] != '\0'; ++length)
 	{
-		tems[length] = s[length];
 	}
 	ptr = s;
+	tems = s;
+	_putchar('\n');
 	while (length--)
 	{
 		*ptr = tems[length];
+		_putchar(tems[length]);
+		_putchar(*ptr);
+		_putchar(' ');
+		_putchar('\n');
 		ptr++;
 	}
 }
-
