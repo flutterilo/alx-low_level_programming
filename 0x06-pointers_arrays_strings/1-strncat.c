a28b9353ed3504af4519c4d516706f3b2b2923fd
Ilyass <ilias.kassimi@gmail.com>
2023-03-29 18:18:45 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* *_strcat - concatenates two string
+* *_strncat - concatenates two string
 * @dest: parameter dest
 * @src: parameter src
 * @n: number of bytes will used
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		*dest = *src;
 		dest++;
-		if(gt == n)
+		if (gt == n)
 		{
 			break;
 		}
