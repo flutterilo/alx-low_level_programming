4b521c5621f41ff34261929f0a47383d1670b3f9
Ilyass <ilias.kassimi@gmail.com>
2023-04-04 01:00:57 +0000

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - print the sum of two diagonals
+* @a: parameter a
+* @size: size
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int diag1 = 0, diag2 = 0;
+	int total_cell = (size * size) - 1;
+
+	while (total_cell)
+	{
+		diag1 += a[total_cell];
+		total_cell -= size +1;
+	}
+	total_cell = (size * size) - 1 - (size -1);
+	while (total_cell)
+	{
+		diag2 += a[total_cell];
+		total_cell -= size -1;
+	}
+	printf("%d, %d\n", diag1, diag2);
+}
