d4d4bc14352ae85faa2155d62703efdbe0e43338
Ilyass <ilias.kassimi@gmail.com>
2023-04-05 17:01:48 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - get length string using recursion
+* @s: string parameter
+* Return: return length;
+*/
+
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
