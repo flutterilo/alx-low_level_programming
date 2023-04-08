b959e05c8d719d17a1456445c1e7a88a8132cca8
Ilyass <ilias.kassimi@gmail.com>
2023-04-08 22:55:30 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,23 +15,23 @@ int num, j, result;
 
 	if (argc != 2)
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 	num = atoi(argv[1]);
 	result = 0;
 	if (num < 0)
 	{
-	printf("0\n");
-	return (0);
+		printf("0\n");
+		return (0);
 	}
 	for (j = 0; j < 5 && num >= 0; j++)
 	{
-	while (num >= coins[j])
-	{
-	result++;
-	num -= coins[j];
-	}
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
 	}
 	printf("%d\n", result);
 	return (0);
