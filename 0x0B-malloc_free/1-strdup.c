7d65d33c83e116d2deb0a081312c83ca9d7bdd39
Ilyass <ilias.kassimi@gmail.com>
2023-04-12 00:07:29 +0000

    1-strup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - copy string to dynamic memory
+* @str: string parameter
+* Return: return pointer to string
+*/
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i = 0,j;
+
+	if (str == NULL)
+		return NULL;
+	while (str[i] != '\0')
+		i++;
+	ptr = malloc(sizeof(char) * i);
+	for(j = 0; j < i; j++)
+	{
+		ptr[j] = str[j];
+	}
+	return (ptr);
+}
