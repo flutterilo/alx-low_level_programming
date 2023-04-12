cfd2402ecc0474327509fc357604d5403c1147e5
Ilyass <ilias.kassimi@gmail.com>
2023-04-12 17:17:25 +0000

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* argstostr - conconate all argument of program
+* @ac: numbers of arguments
+* @av: array of content
+* Return: point to array
+*/
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, j, k, b = 0;
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			k++;
+		}
+	}
+	k += ac;
+	ptr = malloc(sizeof(char) * k + 20);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			ptr[b] = av[i][j];
+			b++;
+		}
+		ptr[b++] = '\n';
+	}
+	ptr[b++] = '\0';
+	return (ptr);
+}
