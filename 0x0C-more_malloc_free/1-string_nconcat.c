25bfd8f0418c40c55b4e5c2b79b07485d746385c
Ilyass <ilias.kassimi@gmail.com>
2023-04-13 22:28:32 +0000

    1-string_nconcate.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,38 +2,37 @@
 #include <stdlib.h>
 
 /**
-* string_nconcat - concatenate two strings
-* @s1: dest string
-* @s2: source string
-* @n: length of string
-* Return: pointer to string
+* string_nconcat - conconating string
+* @s1: string one
+* @s2: string two
+* @n: number of charters in s2
+* Return: return pointer
 */
+
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
-	while (s1 && s1[len1])
+	while (s1[len1])
 		len1++;
-	while (s2 && s2[len2])
+	while (s2[len2])
 		len2++;
-	if (n < len2)
-		ptr = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (n >= len2)
+		n = len2;
+	ptr = malloc(sizeof(char) * (len1 + n + 1));
 	if (ptr == NULL)
 		return (NULL);
-	i = 0;
-	while (i < len1)
+	while (s1[i])
 	{
 		ptr[i] = s1[i];
 		i++;
 	}
-	while (i < (len1 + n) && n < len2)
+	while (j < n)
+	{
 		ptr[i++] = s2[j++];
-	while (n >= len2 && i < (len1 + len2))
-		ptr[i++] = s2[j++];
+	}
 	ptr[i] = '\0';
 	return (ptr);
 }
