e4e536ce2f7554b736ca2da21693017ab4be1a9e
Ilyass <ilias.kassimi@gmail.com>
2023-04-14 17:28:57 +0000

    3-array_range.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _realloc - realocates a memory block
+* @ptr: pointer parameter
+* @old_size: old size parameter
+* @new_size: new size parameter
+* Return: return pointer
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr, *old_ptr;
+	int size, i = 0;
+
+	if (new_size == old_size)
+		return (ptr);
+	size = new_size > old_size ? old_size : new_size;
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		new_ptr = malloc(new_size);
+	else
+		new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (new_ptr);
+
+	old_ptr = ptr;
+	while (i < size)
+	{
+		new_ptr[i] = old_ptr[i];
+		i++;
+	}
+
+	free(ptr);
+	return (new_ptr);
+
+}
+
