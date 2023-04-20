22de6c8c6ba8f70f7b6ab6879774a1b9c159d146
Ilyass <ilias.kassimi@gmail.com>
2023-04-20 14:38:42 +0000

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print opcode of its own main function
+* @argc: number of arguments
+* @argv: content of arguments
+* Return: return depend on sucess
+*/
+
+int main(int argc, char *argv[])
+{
+	char *arr = (char *)main;
+	int num;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	num = atoi(argv[1]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	while(num--)
+	{
+		printf("%02hhx%c", *arr++, num ? ' ' : '\n');
+	}
+	return (0);
+}
