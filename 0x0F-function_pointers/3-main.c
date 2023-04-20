7e47f1440c1c5812b78883049451997f1c46b0aa
Ilyass <ilias.kassimi@gmail.com>
2023-04-20 03:08:13 +0000

    3-calc

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+
+/**
+* main - program that performs simple operations
+* @argc: number of arguments
+* @argv: content of arguments
+* Return: return depend on succss case
+*/
+
+int main(int argc, char *argv[])
+{
+	char *op = argv[2];
+	int num1, num2;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+* op_add - operation addition
+* @a: parameter a
+* @b: parameter b
+* Return: return result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - operation substuction
+* @a: parameter a
+* @b: parameter b
+* Return: return result
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - operation multiplication
+* @a: parameter a
+* @b: parameter b
+* Return: return result
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_dev - operation devision
+* @a: parameter a
+* @b: parameter b
+* Return: return result
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - operation Model
+* @a: parameter a
+* @b: parameter b
+* Return: return result
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
