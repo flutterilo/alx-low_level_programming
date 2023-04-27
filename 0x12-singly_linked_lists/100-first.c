798482b5bbd12beb9d57d01273d16939d47402c1
Ilyass <ilias.kassimi@gmail.com>
2023-04-27 22:43:30 +0000

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* myStartupFun - excute code before main function
+*/
+
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n" +
+				 "I bore my house upon my back!\n");
+}
