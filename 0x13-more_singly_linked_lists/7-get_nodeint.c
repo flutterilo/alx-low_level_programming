7bf0a46d0ba3e3cbcb408cc305f7fb8bc813528b
Ilyass <ilias.kassimi@gmail.com>
2023-05-01 14:34:18 +0000

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - return node in specific index
+* @head: first node of listint_t
+* @index: index parameter
+* Return: return node
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (i < index)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+		i++;
+	}
+	return (head);
+}
