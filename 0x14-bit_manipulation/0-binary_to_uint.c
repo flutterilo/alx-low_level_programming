5c23cb1f5e54148060b6d2849e318bf49e088ff2
Ilyass <ilias.kassimi@gmail.com>
2023-05-04 19:58:17 +0000

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+* _power - cacluate power of number
+* @a: number base
+* @b: exponent number
+* Return: return result
+*/
+
+int _power(int a, int b)
+{
+	int rst = a, i = 1;
+
+	if (b == 0)
+		return (1);
+	while (i < b)
+	{
+		rst = rst * a;
+		i++;
+	}
+	return (rst);
+}
+
+/**
+* binary_to_uint - function that convert binrary to unsigned int
+* @b: string of 1 and 0
+* Return: return converted number
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0, j = 0;
+	int i = 0;
+
+	while (b[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		if (b[i] == '1' || b[i] == '0')
+			sum += (b[i] - '0') * _power(2, j);
+		else
+			return (0);
+		j++;
+		i--;
+	}
+	return (sum);
+}
