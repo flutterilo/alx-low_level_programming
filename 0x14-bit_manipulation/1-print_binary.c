71474aaee555c249bffdaa7a0416fe46dc25d8ab
Ilyass <ilias.kassimi@gmail.com>
2023-05-05 15:17:35 +0000

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_binary - print binary representation of number
+* @n: number parameter
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int check = 1;
+
+	while (check <= n)
+	{
+		check <<= 1;
+	}
+	if (n > 0)
+		check >>= 1;
+	while (check)
+	{
+		if (check & n)
+			_putchar('1');
+		else
+			_putchar('0');
+		check >>= 1;
+	}
+}
