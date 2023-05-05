f785e77085b5f8d5a2c2de0be370a6c722dc558c
Ilyass <ilias.kassimi@gmail.com>
2023-05-05 14:23:22 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
-* _power - cacluate power of number
-* @a: number base
-* @b: exponent number
-* Return: return result
-*/
-
-int _power(int a, int b)
-{
-	int rst = a, i = 1;
-
-	if (b == 0)
-		return (1);
-	while (i < b)
-	{
-		rst = rst * a;
-		i++;
-	}
-	return (rst);
-}
-
-/**
 * binary_to_uint - function that convert binrary to unsigned int
 * @b: string of 1 and 0
 * Return: return converted number
@@ -29,24 +8,14 @@ int _power(int a, int b)
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0, j = 0;
-	int i = 0;
+	unsigned int sum = 0;
 
 	if (!b)
 		return (0);
-	while (b[i] != '\0')
+	while (b)
 	{
-		i++;
-	}
-	i--;
-	while (i >= 0)
-	{
-		if (b[i] == '1' || b[i] == '0')
-			sum += (b[i] - '0') * _power(2, j);
-		else
-			return (0);
-		j++;
-		i--;
+		sum += sum << 1 + b[i] - '0';
+		b >>= 1;
 	}
 	return (sum);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,6 +3,5 @@
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-int _power(int a, int b);
 
 #endif
