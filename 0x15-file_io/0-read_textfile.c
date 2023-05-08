25fd21ec5b411bcb9f3cc70fe16a4d2a7f852b9c
Ilyass <ilias.kassimi@gmail.com>
2023-05-08 18:09:24 +0000

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,28 +13,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *str;
 
 	if (!filename)
-		return (-1);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 	str = malloc(sizeof(char) * letters);
 	if (str == NULL)
 	{
 		close(fd);
-		return (-1);
+		return (0);
 	}
 	check_r = read(fd, str, letters);
 	if (check_r == -1)
 	{
 		free(str);
 		close(fd);
-		return (-1);
+		return (0);
 	}
 	check_w = write(STDOUT_FILENO, str, letters);
 	free(str);
+	close(fd);
 	if (check_w == -1)
-		return (-1);
-	if (close(fd) < 0)
-		return (-1);
+	{
+		return (0);
+	}
 	return (check_r);
 }
