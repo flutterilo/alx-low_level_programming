083bc2eb16f29910a8b58f217ac78e45e6387f5c
Ilyass <ilias.kassimi@gmail.com>
2023-07-07 16:15:51 +0000

    hastable create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+* hash_table_create - create hash table
+* @size: size of hash table
+* Return: return new hash table
+*/
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash;
